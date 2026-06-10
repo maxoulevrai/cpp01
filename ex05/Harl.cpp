64f684160609c62301a1b974828b491dc642b4de
root <root@DESKTOP-AQPOE75.localdomain>
2026-06-10 23:29:53 +0200

    ex05

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -6,23 +6,41 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/10 23:20:07 by root              #+#    #+#             */
-/*   Updated: 2026/06/10 23:21:31 by root             ###   ########.fr       */
+/*   Updated: 2026/06/10 23:23:05 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
-void	complain(std::string level) {
+#include <iostream>
+
+void	Harl::debug( void ) {
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+}
+
+void	Harl::info( void ) {
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void	Harl::warning( void ) {
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void	Harl::error( void ) {
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void	Harl::complain(std::string level) {
 	void (Harl::*funcs[4])() = {
-		&Karen::debug,
-		&Karen::info,
-		&Karen::warning,
-		&Karen::error
+		&Harl::debug,
+		&Harl::info,
+		&Harl::warning,
+		&Harl::error
 	};
 	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 
 	for (int i = 0; i < 4; i++) {
 		if (levels[i] == level) {
-			(this->*funcs[i])(); // appel via le pointeur
+			(this->*funcs[i])();
 			return;
 		}
 	}
